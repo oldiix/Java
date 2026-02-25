f74eeef62c3dddbbc4737cb129c49f98aa5668de
Kateryna <dix.collab@gmail.com>
2026-02-25 15:59:49 +0200

    add 1st lab "Intro to Java": data types and different and cycles

---
+++ labs/Intro to Java/PrimitiveDataTypes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte x = 122;
+        x += 5;
+
+        System.out.println("The result is (after adding 5): " + x);
+    }
+}
---
+++ labs/Intro to Java/if_else/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int grade = 51;
+
+        if (grade >= 50) {
+            System.out.println("Здано!");
+        } else {
+            System.out.println("Помилка.");
+        }
+    }
+}
