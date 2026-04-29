17a99df68a347eca726ba89198897c3608e0d3b7
Kateryna <dix.collab@gmail.com>
2026-04-29 13:38:54 +0300

    add application test

---
+++ labs/lab7_springBoot_hospital/medical_laboratory/src/test/java/org/example/medical_laboratory/MedicalLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medical_laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalLaboratoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
