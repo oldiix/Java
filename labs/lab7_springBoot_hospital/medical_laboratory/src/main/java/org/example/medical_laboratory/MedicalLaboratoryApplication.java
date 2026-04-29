2dfcad60c66173cd1dc6af4d46afae505b56934e
Kateryna <dix.collab@gmail.com>
2026-04-29 13:38:37 +0300

    add main application entry point

---
+++ labs/lab7_springBoot_hospital/medical_laboratory/src/main/java/org/example/medical_laboratory/MedicalLaboratoryApplication.java
@@ -0,0 +1,11 @@
+package org.example.medical_laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalLaboratoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalLaboratoryApplication.class, args);
+    }
+}
